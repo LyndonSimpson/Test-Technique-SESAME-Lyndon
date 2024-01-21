e1fc23bd04ab682391dbbde10557232c3dbcb06d
LyndonSimpson <lyndon.simpson@gmail.com>
2024-01-21 15:56:28 +0100

    fetch articles OK

--- API/app/controller/articles.js
+++ API/app/controller/articles.js
@@ -1,5 +1,25 @@
 const fetch = require('node-fetch');
 
-const getArticles = async (req, res) => {}
+const getArticles = async (req, res) => {
+    try {
+        const builderIoUrl = 'https://cdn.builder.io/api/v3/content/articles?apiKey=6e139f4f71454a88b3f01ee85b1a35b5';
+
+        const response = await fetch(builderIoUrl);
+
+        if (response.status === 200) {
+            const data = await response.json();
+            const articles = data.results[0].data.articles;
+
+            res.json({
+                articles: articles
+            });
+        } else {
+            res.status(response.status).send('Error fetching articles');
+        }
+    } catch (error) {
+        console.error('Error in getArticles controller:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
 
 module.exports = { getArticles };
